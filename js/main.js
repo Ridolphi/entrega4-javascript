21955802967dd145e78fa5bb62cd9a2e48129ee1
Rafaela Ridolphi <rafaela-ridolphi@hotmail.com>
2024-06-15 14:00:43 +0200

    final commit

--- js/main.js
+++ js/main.js
@@ -1,24 +1,27 @@
 const sections = document.querySelectorAll("[data-src]");
 for (const section of sections) {
-  const response = await fetch(section.dataset.src); // cogemos todos las secciones con el dataset-src de la main
-  const mainContent = await response.text();  // pasamos la respuesta obtenida de cada archivo a texto plano
-  section.outerHTML = mainContent;        // la mostramos por el navegador sustituyendo cada seccion por su                                         archivo referenciado en el data-src
+  const response = await fetch(section.dataset.src); 
+  const mainContent = await response.text();  
+  section.outerHTML = mainContent;        
 }
 
-const form = document.querySelector("form");
-console.log(form);
-form.addEventListener("submit", function (event) {
-  let email = document.getElementById("inputEmail").value;
-  let phone = document.getElementById("inputPhone").value;
-  let emailPattern = /@gmail\./i;
-  if (emailPattern.test(email)) {
-    alert("Your address can not be @gmail ❌❌❌❌");
-    event.preventDefault();
-  }
-  let phonePattern = /^[679]\d{8}(\s.*)?$/;
-  if (!phonePattern.test(phone)) {
-    alert("Your phone number is not valid ❌❌❌❌");
-    event.preventDefault();
-  }
+cdocument.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('myForm').addEventListener('submit', function (event) {
+    let emailInput = document.querySelector('#myForm input[type="email"]').value;
 
+    if (emailInput.includes('@gmail.com')) {
+      alert('Gmail addresses are not allowed.');
+      event.preventDefault();
+      return false;
+    }
+
+    let phoneInput = document.querySelector('#myForm input[type="tel"]').value;
+    let phoneRegex = /^(6|7|9)[0-9]{8})$/;
+    if (!phoneRegex.test(phoneInput.replace(/\s+/g, ''))) {
+      alert('Number must have 9 digits, starting with 6, 7 y 9.');
+      event.preventDefault();
+      return false;
+    }
+
+  });
 });
