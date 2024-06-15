f43b4213450f00e051c5e44650adbe8a2671b80b
Rafaela Ridolphi <rafaela-ridolphi@hotmail.com>
2024-06-15 14:19:12 +0200

    last

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-const sections = document.querySelectorAll("[data-src]");
-for (const section of sections) {
-  const response = await fetch(section.dataset.src); 
-  const mainContent = await response.text();  
-  section.outerHTML = mainContent;        
-}
+document.addEventListener('DOMContentLoaded', async function () {
+  const sections = document.querySelectorAll("[data-src]");
+  for (const section of sections) {
+    const response = await fetch(section.dataset.src);
+    const mainContent = await response.text();
+    section.outerHTML = mainContent;
+  }
 
-document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('myForm').addEventListener('submit', function (event) {
     let emailInput = document.querySelector('#myForm input[type="email"]').value;
 
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     let phoneInput = document.querySelector('#myForm input[type="tel"]').value;
-    let phoneRegex = /^(6|7|9)[0-9]{8})$/;
+    let phoneRegex = /^(6|7|9)[0-9]{8}$/; 
     if (!phoneRegex.test(phoneInput.replace(/\s+/g, ''))) {
       alert('Number must have 9 digits, starting with 6, 7 y 9.');
       event.preventDefault();
